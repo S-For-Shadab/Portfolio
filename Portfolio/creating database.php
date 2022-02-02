fba5f7d64141a4a00f4c43c486a0e0801f1cd69d
S-For-Shadab <96187941+S-For-Shadab@users.noreply.github.com>
2022-02-02 12:43:27 +0530

    Add files via upload

---
+++ Portfolio/creating database.php
@@ -0,0 +1,17 @@
+<?php
+// $servername="localhost";
+// $username="root";
+// $password="";
+// $conn=mysqli_connect($servername,$username,$password);
+
+// $sql="CREATE DATABASE dbharry1";
+// $result= mysqli_query($conn,$sql);
+// echo var_dump($result);
+// if($result){
+// echo "data is successfull inserted!!";
+// }else{
+//     die("sorry we fail to insert you data due to :".mysqli_connect_error());
+    
+// }
+echo "hello world";
+?>
---
+++ Portfolio/public/index.js
@@ -0,0 +1,6 @@
+document.querySelector(".container").addEventListener("click",abc);
+function abc(){
+    document.querySelector("div").classList.add("xyz");
+    
+}
+// document.getElementsByClassName("container").style.background="yellow";
